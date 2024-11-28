7edea62ba4598f9a000dd87a1ca55fa6deaf340c
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-11-28 17:03:52 +0100

    quick fix for push

--- setter-getter/target/debug/deps/setter_getter-c78d247bcaf32f2d.d
+++
@@ -1,5 +0,0 @@
-/home/suleiman/Downloads/RUST/GITHUB-RUST/rust-adventures/setter-getter/target/debug/deps/setter_getter-c78d247bcaf32f2d: src/main.rs
-
-/home/suleiman/Downloads/RUST/GITHUB-RUST/rust-adventures/setter-getter/target/debug/deps/setter_getter-c78d247bcaf32f2d.d: src/main.rs
-
-src/main.rs:
