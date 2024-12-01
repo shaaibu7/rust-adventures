db0a0f8ea76dd8e14c0786099f2a2ae6ddb662ac
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-12-01 04:29:27 +0100

    version control fix

--- fibonacci_sequence/target/debug/deps/fibonacci_sequence-df34d49befedab6e.d
+++
@@ -1,5 +0,0 @@
-/home/suleiman/Downloads/RUST/GITHUB-RUST/rust-adventures/fibonacci_sequence/target/debug/deps/fibonacci_sequence-df34d49befedab6e: src/main.rs
-
-/home/suleiman/Downloads/RUST/GITHUB-RUST/rust-adventures/fibonacci_sequence/target/debug/deps/fibonacci_sequence-df34d49befedab6e.d: src/main.rs
-
-src/main.rs:
--- fibonacci_sequence/target/debug/fibonacci_sequence.d
+++
@@ -1,1 +0,0 @@
-/home/suleiman/Downloads/RUST/GITHUB-RUST/rust-adventures/fibonacci_sequence/target/debug/fibonacci_sequence: /home/suleiman/Downloads/RUST/GITHUB-RUST/rust-adventures/fibonacci_sequence/src/main.rs
